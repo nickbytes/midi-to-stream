91b71dd6f548fd62d2b0df2c1c56214a3b9e4da5
Nick Beattie <nick@nickbytes.com>
2017-08-12 12:31:09 -0400

    testing observing master value from ableton

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MIDI to Stream
 Process MIDI and output to Javascript stream
+
+Values to observe
+- master volume
+- track volume
+- velocity from track played, if not velocity, just a hit... but maybe figured out by volume differences in track
+- filter/effect level
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("midi");
+import Max4Node from "max4node";
+
+const max = new Max4Node();
+max.bind();
+
+max
+  .observe({
+    path: "live_set master_track mixer_device volume",
+    property: "value"
+  })
+  .on("value", function(val) {
+    console.log("Master track volume: " + val);
+  });
